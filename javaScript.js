40a446255e8b5b53ad59b31c909c91297c4e9b82
tolyan160582 <tolyan160582@gmail.com>
2022-03-17 18:41:25 +0200

    change all files

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,8 @@
+function showClass(){
+    let elem=document.getElementsByClassName("blue");
+    for(let key of elem){
+        key.style="background-color:green";
+    }
+    document.getElementById("lorem").style="background-color:pink";
+}
 
